ee522807767a27960b00c2aacaede36d6caac732
Flora Murakeozy-Kis <fmurakeozykis@gmail.com>
2025-09-08 13:04:06 +0200

    improve structure

--- main.R
+++ main.R
@@ -5,7 +5,7 @@ library(readr)
 
 #setwd("C:/Users/fmura/Documents/groningen/Hon Project Arctic Algae/Data/R/csvs_for_chat")
 
-#RERUNS!!
+#RERUNS!! 
 pol <- read_excel("data/raw/Data_Final_FMK.xlsx", sheet = 1, skip = 4)
 chr <- read_excel("data/raw/Data_Final_FMK.xlsx", sheet = 2, skip = 4) 
 
@@ -39,6 +39,23 @@ write_csv(pol, file.path("data", "processed", "clean_pol.csv"))
 chr <- clean_names(chr)
 chr <- remove_empty(chr, which = c("rows", "cols"))
 write_csv(chr, file.path("data", "processed", "clean_chr.csv"))
+
+pol <- read_csv("clean_pol.csv")
+
+# Add replicate C row with only known fields + μ-POC; others NA
+new_row <- tibble(
+  species    = "Polarella",
+  sal_adapt  = 45,
+  sal_exp    = 25,
+  time       = 24,
+  repl       = "C",
+  id         = "DMSPp",
+  u_poc_h_1  = 0.0029
+)
+
+# Bind and save
+pol_mod <- bind_rows(pol, new_row)
+write_csv(pol, file.path("data", "processed", "clean_pol_modified.csv"))
 
 
 ###############################################################
